e7185d76b523661c70ec8e8f36f3075af1906f05
Paloma <paloma.toledo@redspark.io>
2024-10-14 12:38:25 -0300

    Primeiro projeto de estudos iniciado

---
+++ src/main/kotlin/com/example/estudos/EstudosApplication.kt
@@ -0,0 +1,11 @@
+package com.example.estudos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EstudosApplication
+
+fun main(args: Array<String>) {
+	runApplication<EstudosApplication>(*args)
+}
